7daa1871c43ff3637c650bafea31099dfc3cdedb
lokendra sondhiya <lokendra.sondhiya@gmail.com>
2025-12-19 23:20:49 +0530

    Add comments and total count output to list_count_print.py

--- list_count_print.py
+++ list_count_print.py
@@ -1,6 +1,8 @@
+# Counting iterations in nested loops and printing values
 count = 0
 for x in [0, 2, 1, 3]:
     for y in [0, 4, 1, 2]:
         print('x')
         count = count + 1
         print(count, ":", x, y)
+print("Total iterations:", count)   # Output: 16    
