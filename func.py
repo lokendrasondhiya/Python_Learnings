edc983ca046a0afc391ad0050708fcc0e254d0c6
lokendra sondhiya <lokendra.sondhiya@gmail.com>
2025-12-19 23:26:00 +0530

    Remove func.py

--- func.py
+++
@@ -1,7 +0,0 @@
-def multi_func(num1, num2):
-    print(f"Inside function: num1={num1}, num2={num2}")
-    return num1 * num2
-
-result = multi_func(5, 10)
-print(f"Result: {result}")
-                      
